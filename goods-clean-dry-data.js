5726d696c5237488015a11bafa0d2d9e93d49d5b
Foxfonblack <47613239+Foxfonblack@users.noreply.github.com>
2024-08-19 11:02:30 +0300

    190824-1102

---
+++ goods-clean-dry-data.js
@@ -0,0 +1,25 @@
+const goodsData=[
+    {
+        convectors:[
+            // Winia Мийка повітря
+            {brandName:"Winia AWM-40PTWC", 
+                partNo: "0102022859-100439363"},
+
+            // Зволожувач ультразвуковий
+            {brandName:"iClima LUX-606HW", 
+                partNo: "0102060154-100440148"},
+            {brandName:"Leberg LH-206W", 
+                partNo: "0102060121-100437917"},
+
+            // Осушувачі компрессорні Neoclima
+            {brandName:"Neoclima SBN-012", 
+                partNo: "0105012801-100437462"}, 
+            {brandName:"Neoclima SBN-20", 
+                partNo: "0105012801-100437463"},
+            {brandName:"Neoclima SBN-30", 
+                partNo: "0105012801-100437464"},
+            // Осушувачі компрессорні Neoclima
+
+            ]
+        }
+    ]
---
+++ goods-vent-data.js
@@ -0,0 +1,43 @@
+const goodsData=[
+    {
+        convectors:[
+            // Roda Припливно-витяжна установка моноблочна
+            {brandName:"Roda LMW-1300N", 
+                partNo: "0206030319-000418638"},
+            {brandName:"Roda LMW-500N", 
+                partNo: "0206030319-000418638"},
+            {brandName:"Roda LMW-800N", 
+                partNo: "0206030319-000418638"},
+
+            // Вентилятор осьовий
+            {brandName:"OSTBERG CK 250 B ErP", 
+                partNo: "0206010130-100421938"},
+            // Вентилятор канальний осьовий
+
+
+            // Вентилятор канальний осьовий
+            {brandName:"OSTBERG CK 100 A", 
+                partNo: "0206010630-000026921"}, 
+            {brandName:"OSTBERG CK 125 A", 
+                partNo: "0206010630-000026924"},
+            {brandName:"OSTBERG CK 125 C", 
+                partNo: "0206010630-000026925"},
+            {brandName:"OSTBERG CK 150 B", 
+                partNo: "0206010630-000026926"}, 
+            {brandName:"OSTBERG CK 250 A", 
+                partNo: "0206010630-000026932"},
+            {brandName:"OSTBERG LPK 200 A", 
+                partNo: "0206010630-000026943"},
+            // Вентилятор канальний осьовий
+
+
+            // Клапани
+            {brandName:"OSTBERG RSK 200", 
+                partNo: "0206060030-100422210"},
+            {brandName:"OSTBERG RSK 315", 
+                partNo: "0206060030-100422211"}, 
+            // Клапани
+
+            ]
+        }
+    ]
